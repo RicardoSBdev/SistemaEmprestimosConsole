0ff43d857478389ec2f1640df349486d4f24008a
Ricardo Bueno <ricardo.sb.contato@gmail.com>
2025-10-24 10:39:48 -0300

    commit aula 1

---
+++ SistemaEmprestimosCursoConsole/Models/Emprestimo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaEmprestimosCursoConsole.Models
+{
+    internal class Emprestimo
+    {
+        public int Id { get; set; }
+        public int LivroId { get; set; }
+        public int UsuarioId { get; set; }
+        public DateTime DataEmprestimo { get; set; } = DateTime.Now;
+        public DateTime DataDevolucao { get; set; }
+
+    }
+}
---
+++ SistemaEmprestimosCursoConsole/Models/Livros.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaEmprestimosCursoConsole.Models
+{
+    internal class Livros
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public bool Disponivel { get; set; } = true;
+
+
+
+    }
+}
